db836828382b3b246c26afcd8c63f9004699faac
Damien <ddubost6998@gmail.com>
2025-03-12 11:30:27 +0100

    Update user entity

--- src/app/entities/user.entity.ts
+++ src/app/entities/user.entity.ts
@@ -1,42 +1,54 @@
+import {Address, AddressHttp} from "./address.entity";
+
 export interface UserHttp {
   id: number;
   email: string;
-  password: string;
+  password?: string;
   firstname: string;
   lastname: string;
-  phone: string;
+  phone?: string | null;
   birthdate: string;
-  verificationCode: string;
+  verificationCode?: string | null;
   isVerified: boolean;
-  address: string;
+  address: AddressHttp;
 }
 
 export interface User {
   id: number;
   email: string;
-  password: string;
   firstname: string;
   lastname: string;
-  phone: string;
-  birthdate: string;
-  verificationCode: string;
+  phone?: string | null;
+  birthdate: Date;
   isVerified: boolean;
-  address: string;
+  address: Address;
 }
 
 export namespace User {
-  export function fromHttp(user: UserHttp): User {
+  export function fromHttp(userHttp: UserHttp): User {
+    return {
+      id: userHttp.id,
+      email: userHttp.email,
+      firstname: userHttp.firstname,
+      lastname: userHttp.lastname,
+      phone: userHttp.phone,
+      birthdate: new Date(userHttp.birthdate),
+      isVerified: userHttp.isVerified,
+      address: Address.fromHttp(userHttp.address),
+    };
+  }
+
+  export function toHttp(user: User): UserHttp {
     return {
       id: user.id,
       email: user.email,
-      password: user.password,
       firstname: user.firstname,
       lastname: user.lastname,
       phone: user.phone,
-      birthdate: user.birthdate,
-      verificationCode: user.verificationCode,
+      birthdate: user.birthdate.toISOString(),
       isVerified: user.isVerified,
-      address: user.address,
-    };
+      address: Address.toHttp(user.address)
+
+    }
   }
 }
