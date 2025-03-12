7232984c5cdafa384e62f7ed45cf10ef2e22cdfd
Damien <ddubost6998@gmail.com>
2025-03-12 12:20:35 +0100

    Add interceptors auth and errors

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthInterceptor } from './interceptors/auth.interceptor';
+import { ErrorInterceptor } from './interceptors/error.interceptor';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    HttpClientModule
-  ]
+  imports: [CommonModule, HttpClientModule],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+  ],
 })
-export class CoreModule { }
+
+export class CoreModule {
+}
--- src/app/core/interceptors/error.interceptor.ts
+++ src/app/core/interceptors/error.interceptor.ts
@@ -1,5 +1,35 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
-export const errorInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
-};
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+
+  constructor(private authService: AuthService, private router : Router) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    return next.handle(request).pipe(
+      catchError((error: HttpErrorResponse) => {
+        let errorMessage = '';
+
+        if (error.error instanceof ErrorEvent) {
+          errorMessage = `Erreur: ${error.error.message}`;
+        } else {
+          errorMessage = `Code d'erreur: ${error.status}, Message: ${error.message}`;
+          if (error.status === 401) {
+            this.authService.logout();
+            this.router.navigate(['/auth/login']);
+            errorMessage = "Votre session a expiré. Veuillez vous reconnecter.";
+          } else if(error.status === 403){
+            errorMessage = "Accès refusé"
+          }
+        }
+        console.error(errorMessage);
+        return throwError(() => errorMessage);
+      })
+    );
+  }
+}
