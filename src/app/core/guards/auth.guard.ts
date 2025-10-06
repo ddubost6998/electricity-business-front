a05a0dad8a4882dbf9fa361345d87820755f6a39
Damien <ddubost6998@gmail.com>
2025-10-06 16:46:39 +0200

    Refactor authentication and JWT handling

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,5 +1,35 @@
-import {CanActivateFn} from '@angular/router';
+import {inject} from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivateFn, Router, RouterStateSnapshot} from '@angular/router';
+import {AuthService} from '../services/auth.service';
+import {jwtDecode, JwtPayload} from 'jwt-decode';
 
-export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+const authGuard: CanActivateFn = (_route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
+    const authService = inject(AuthService);
+    const router = inject(Router);
+
+    const token = authService.getToken();
+
+    if (!token) {
+        router.navigate(['/login'], {queryParams: {returnUrl: state.url}});
+        return false;
+    }
+
+    try {
+        const decoded = jwtDecode<JwtPayload & { exp: number }>(token);
+        const now = Math.floor(Date.now() / 1000);
+
+        if (decoded.exp && decoded.exp < now) {
+            authService.logout(); // Supprime le token expiré
+            router.navigate(['/login'], {queryParams: {returnUrl: state.url}});
+            return false;
+        }
+
+        return true; // Token valide, accès autorisé
+    } catch (error) {
+        console.error('Invalid token:', error);
+        authService.logout();
+        router.navigate(['/login'], {queryParams: {returnUrl: state.url}});
+        return false;
+    }
 };
+export default authGuard
